a36fbbc9ea1ded4669f731299cb54489fffcb6c7
Jeremy German <jgerman@crusader.local>
2015-10-21 09:15:34 -0400

    Updated with login.

--- src/roll20_script_sync/core.clj
+++ src/roll20_script_sync/core.clj
@@ -1,11 +1,9 @@
 (ns roll20-script-sync.core
-  (:require [carica.core :refer [configurer resources overrider]]
+  (:require [carica.core :refer [config configurer resources overrider]]
             [clj-webdriver.taxi :refer :all]
             [clojure-watch.core :refer [start-watch]]))
 
 
-(def config (configurer "test.edn"))
-(def override-config (overrider config))
 (def driver (new-driver {:browser :firefox}))
 
 
@@ -13,17 +11,21 @@
   (cond (.endsWith "/" dir) (str dir f)
         :else (str dir "/" f)))
 
+
 (defn -main [& args]
   ;; we'll clean this bit up later
   (let [monitor-dir (first args)
-        config (configurer (make-path monitor-dir "api-scripts.edn"))
+        config (configurer [(make-path (. System getProperty "user.home") ".roll20.edn")
+                            (make-path monitor-dir "api-scripts.edn")])
         override-config (overrider config)
         campaign-url (config :campaign-url)
         files (config :files)]
+    (to driver campaign-url) ;; really this one will take you to login
+    (input-text driver "label+input[name=email]" (config :username))
+    (input-text driver "label+input[name=password]" (config :password))
+    (click driver "button.btn.btn-primary.calltoaction.btn-lg")
     (to driver campaign-url)
-    (println "Hit enter when logged in...")
-    (read-line)
-    (to driver campaign-url)
+    #_(println (second (elements driver ".script, .tab-pane")))
     )
 
   )
