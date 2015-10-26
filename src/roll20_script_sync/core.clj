514bfcabc5b59c06d17af4d40d9cc52217aa29ff
Jeremy German <jeremy.german@gmail.com>
2015-10-26 07:18:24 -0400

    checkin

--- src/roll20_script_sync/core.clj
+++ src/roll20_script_sync/core.clj
@@ -1,7 +1,8 @@
 (ns roll20-script-sync.core
   (:require [carica.core :refer [config configurer resources overrider]]
             [clj-webdriver.taxi :refer :all]
-            [clojure-watch.core :refer [start-watch]]))
+            [clojure-watch.core :refer [start-watch]]
+            [clojure.string :as str]))
 
 
 (def driver (new-driver {:browser :firefox}))
@@ -11,6 +12,20 @@
   (cond (.endsWith "/" dir) (str dir f)
         :else (str dir "/" f)))
 
+(defn get-script-tabs [driver]
+  (elements driver "a[data-toggle=tab]"))
+
+(defn script-tabs-to-dict [script-tabs]
+  (into {} (map (fn [x] {(text x) x}) script-tabs)))
+
+(defn extract-script-id [element]
+  (-> (attribute element :href)
+      (str/split #"#")
+      (second)))
+
+;; get a text area div#script-37306>textarea
+
+(defn map-script-ids )
 
 (defn -main [& args]
   ;; we'll clean this bit up later
